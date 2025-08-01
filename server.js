c8ce1b8cae75e8adea5300dc99c4900219b890db
Alexis Bustos <alexisbustos10@yahoo.com>
2025-07-31 20:05:43 -0700

    implemented an info page that shows the time that the request was received and how many entries are in the phonebook at that time

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ app.get("/api/persons", (req, res) => {
   res.json(phonebook);
 });
 
+app.get("/info", (req, res) => {
+  // using <br> to define a line break Express will set the default header
+  // to text/html. If you want to use the newline char, then you need to
+  // set the Content-Type to text/plain with res.set('Content-Type', 'text/plain')
+  res.send(
+    `Phonebook has info for ${phonebook.length} people<br> ${new Date()}`
+  );
+});
+
 app.listen(process.env.PORT || PORT, () =>
   console.log(`Server is running on port ${PORT}...`)
 );
